f62581dc30f27238fe5b4a3484ee9dc3f1ebd7ca
Akash <akashdeep.samantra@protonmail.com>
2024-02-24 02:00:20 +0530

    Baseline config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    priority = 1000,
+  },
+}
+
+--return {
+--  "catppuccin/nvim",
+--  lazy = true,
+--  name = "catppuccin",
+--  priority = 1000,
+-- }
