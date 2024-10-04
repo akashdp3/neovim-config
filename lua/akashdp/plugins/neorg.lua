11dea66d55ccd0dee2e6a67fad7ffbae53cee33c
akashdp3 <akashdeep.samantra@protonmail.com>
2024-10-04 15:51:56 +0530

    Add neorg to neovim config

--- lua/akashdp/plugins/neorg.lua
+++ lua/akashdp/plugins/neorg.lua
@@ -1,40 +1,40 @@
 return {
-  {
-    "vhyrro/luarocks.nvim",
-    priority = 1000,
-    config = true
-  },
-  {
-    "nvim-neorg/neorg",
-    ft = "norg",
-    build = ":Neorg sync-parsers",
-    dependencies = {
-      "luarocks.nvim",
-      { "nvim-neorg/neorg-telescope" }
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1000,
+        config = true
     },
-    config = function ()
-      require("neorg").setup {
-        load = {
-          ["core.defaults"] = {},
-          ["core.concealer"] = {},
-          ["core.dirman"] = {
-            config = {
-              workspaces = {
-                notes = "~/OneDrive\\ -\\ 7-Eleven,\\ Inc/garden"
-              }
-            }
-          },
-          -- ["core.presenter"] = {},
-          ["core.integrations.telescope"] = {
-            config = {
-                insert_file_link = {
-                    show_title_preview = true
+    {
+        "nvim-neorg/neorg",
+        ft = "norg",
+        build = ":Neorg sync-parsers",
+        dependencies = {
+            "luarocks.nvim",
+            { "nvim-neorg/neorg-telescope" }
+        },
+        config = function ()
+            require("neorg").setup {
+                load = {
+                    ["core.defaults"] = {},
+                    ["core.concealer"] = {},
+                    ["core.dirman"] = {
+                        config = {
+                            workspaces = {
+                                notes = "~/OneDrive\\ -\\ 7-Eleven,\\ Inc/garden"
                             }
+                        }
+                    },
+                    -- ["core.presenter"] = {},
+                    ["core.integrations.telescope"] = {
+                        config = {
+                            insert_file_link = {
+                                show_title_preview = true
+                            }
+                        }
+                    },
+                    -- ["core.norg.journal"] = {}
+                }
             }
-          },
-          -- ["core.norg.journal"] = {}
-        }
-      }
-    end
-  }
+        end
+    }
 }
